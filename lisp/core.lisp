3f555fa83f8655c758457821db5296ac93d17547
tupu910 <tupu910@gmail.com>
2026-06-12 02:30:00 -0300

    Update core.lisp

--- lisp/core.lisp
+++ lisp/core.lisp
@@ -61,6 +61,57 @@
 		)
 	)
 )
+;REQUERIMIENTO 4:
+
+;; ========================================================
+;; FUNCIÓN: duracion-ciclo
+;; NATURALEZA: Pura (Calcula la duración total de un ciclo semafórico)
+;; ESTRATEGIA: Utiliza operaciones aritméticas simples para sumar
+;;             las duraciones de cada estado.
+;; IMPACTO: No destructiva
+;; ========================================================
+(defun duracion-ciclo (rojo amarillo verde)
+	;recibe como parametro el valor de segundos de cada color
+  (+ (* 2 rojo) amarillo verde )
+	;calcula la duracion de un ciclo 
+  )
+
+;; ========================================================
+;; FUNCIÓN: recomendacion-ciclo
+;; NATURALEZA: Pura (Evalúa la duración del ciclo y retorna una recomendación)
+;; ESTRATEGIA: Utiliza la estructura condicional COND para determinar
+;;             si la duración se encuentra dentro de los rangos recomendados.
+;; IMPACTO: No destructiva
+;; ========================================================
+(defun recomendacion-ciclo (duracion)
+	;recibe la duracion del ciclo 
+  (cond 
+    ((< (duracion) 35)
+     "Ciclo demasiado corto")
+		; evalua si la duracion del ciclo es menor a 35 en caso de serlo devuelve "Ciclo demasiado corto"
+    ((<= (duracion) 150)
+     "Ciclo en rango optimo")
+		; evalua si la duracion del ciclo es igual o menor a 150 en caso de serlo devuelve "Ciclo en rango optimo"
+    (t
+     "Ciclo demasiado largo")
+	; en caso de que ninguna de las condiciones anteriores se cumpla devuelve  "Ciclo demasiado largo"
+    )
+)
+;REQUERIMIENTO 5:
+
+;; ========================================================
+;; FUNCIÓN: ciclos-por-tiempo
+;; NATURALEZA: Pura (Calcula la cantidad de ciclos en un período)
+;; ESTRATEGIA: Convierte minutos a segundos y divide por la
+;;             duración total de un ciclo semafórico.
+;; IMPACTO: No destructiva
+;; ========================================================
+(defun ciclos-por-tiempo (minutos)
+	;recibe los minutos 
+  (floor(/ (* minutos 60)
+     (duracion-ciclo ))))
+; calcula la cantidad de ciclos que se realizan por minuto y utiliza la funcionn floor 
+; para redondear al menor numero entrero mas cercano  
 
 ;REQUERIMIENTO 6:
 ;; ========================================================
